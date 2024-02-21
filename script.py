242e70afd0c3446c0ea957c77af0eaf72e1a3fae
Adam Gaffney <adamgaffney96@gmail.com>
2024-02-21 12:26:39 +0000

    Initialised decoder

---
+++ script.py
@@ -0,0 +1,26 @@
+def decode(string: str) -> str:
+    string = string.lower()
+    alphabet: str = "abcdefghijklmnopqrstuvwxyz"
+    decoded_string: str = ""
+    for letter in string:
+        letter_index = alphabet.find(letter)
+        if letter == " ":
+            decoded_string += letter
+        elif letter_index % 2 == 0:
+            decoded_string += alphabet[letter_index+1]
+        else:
+            decoded_string += alphabet[letter_index-1]
+    return decoded_string
+
+def main(message: str) -> None:
+    decoded_message = decode(message)
+    print(f"The input message was '{message}'. The encoded/decoded message is '{decoded_message}'")
+    
+if __name__ == "__main__":
+    print("===========================================")
+    print("Welcome to the Shipwrecked 64 text decoder!")
+    print("===========================================")
+    print("Note that both encoding and decoding a message use the same cypher.\n")
+    message = input("What message would you like to decode/encode: ")
+    main(message)
+    input("Press Enter to close.")

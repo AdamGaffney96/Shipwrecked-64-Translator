15da8247dac281d39fa27cecb9c9aefc11923806
Adam Gaffney <adamgaffney96@gmail.com>
2024-02-21 12:30:32 +0000

    Added repeated decoding functionality and some flavour text

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ def decode(string: str) -> str:
     decoded_string: str = ""
     for letter in string:
         letter_index = alphabet.find(letter)
-        if letter == " ":
+        if letter not in alphabet:
             decoded_string += letter
         elif letter_index % 2 == 0:
             decoded_string += alphabet[letter_index+1]
@@ -21,6 +21,10 @@ if __name__ == "__main__":
     print("Welcome to the Shipwrecked 64 text decoder!")
     print("===========================================")
     print("Note that both encoding and decoding a message use the same cypher.\n")
-    message = input("What message would you like to decode/encode: ")
-    main(message)
-    input("Press Enter to close.")
+    while True:
+        message = input("What message would you like to decode/encode: ")
+        main(message)
+        another = input("Would you like to decode another message (y/n)? ").lower()
+        if (another == "n" or another == "n"):
+            input("Press Enter to close.")
+            break
